21574217b5603393af04b26ff5921d528641aac3
tinotendame <tinotendame@gmail.com>
2022-10-23 06:35:30 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,14 @@
  * code by Tino
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	int b;
 	long int c;
 	long long int d;
 	char a;
 	float e;
+	
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
